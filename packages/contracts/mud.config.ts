b8811c29a413d4e8a61121b6b04d83d5680b6a16
johnpaulcas <casjp.app@gmail.com>
2024-06-04 09:27:50 +0800

    :art: Update formatting

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,37 +1,37 @@
-import { mudConfig } from "@latticexyz/world/register";
+import { mudConfig } from '@latticexyz/world/register'
 
 export default mudConfig({
-  namespace: "bdArena",
+  namespace: 'action',
   systems: {
     KingOfTheHill: {
-      name: "KingOfTheHill",
+      name: 'KingOfTheHill',
       openAccess: true,
-    }
+    },
   },
   tables: {
     KingOfTheHillConfig: {
       keySchema: {
-        smartObjectId: "uint256"
+        smartObjectId: 'uint256',
       },
       valueSchema: {
-        duration: "uint256",
-        expectedItemId: "uint256", // item required
-        expectedItemIncrement: "uint256",
-        lastStartTime: "uint256" // based on startTime
+        duration: 'uint256',
+        expectedItemId: 'uint256', // item required
+        expectedItemIncrement: 'uint256',
+        lastStartTime: 'uint256', // based on startTime
       },
     },
     KingOfTheHillStatus: {
       keySchema: {
-        smartObjectId: "uint256"
+        smartObjectId: 'uint256',
         // id can be startTime
       },
       valueSchema: {
-        king: "address",
-        startTime: "uint256", // to calculate total running time
-        lastClaimedTime: "uint256",
-        totalItemCount: "uint256", // total pot
-        claimed: "bool"
+        king: 'address',
+        startTime: 'uint256', // to calculate total running time
+        lastClaimedTime: 'uint256',
+        totalItemCount: 'uint256', // total pot
+        claimed: 'bool',
       },
-    }
+    },
   },
-});
+})
