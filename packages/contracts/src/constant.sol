20631f7f442b31b456ca29bbda4d8f65005dba68
johnpaulcas <casjp.app@gmail.com>
2024-06-20 06:26:46 +0800

    :recycle: Change constant to constants

--- packages/contracts/src/constant.sol
+++
@@ -1,5 +0,0 @@
-bytes16 constant AREA_CONTROL_LOBBY_SYSTEM_NAME = "AreaControlLobby";
-bytes16 constant AREA_CONTROL_POINT_SYSTEM_NAME = "AreaControlPoint";
-
-// namespace
-bytes14 constant KOTH_NAMESPACE = "hack007";
---
+++ packages/contracts/src/systems/constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+//** System Name */
+
+bytes16 constant AC_LOBBY_SYSTEM_NAME = "AreaControlLobby";
+bytes16 constant AC_POINT_SYSTEM_NAME = "AreaControlPoint";
+
+// ** System Name */
+
+bytes14 constant KOTH_NAMESPACE = "kothTestV4";
