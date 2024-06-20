16044c706f16b2f4914acc03057b513b8bdf12c7
johnpaulcas <casjp.app@gmail.com>
2024-06-20 10:10:30 +0800

    :sparkles: Change namespace

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,7 +1,7 @@
 import { mudConfig } from '@latticexyz/world/register'
 
 export default mudConfig({
-  namespace: 'kothTestV4',
+  namespace: 'kothTestV5',
   systems: {
     KingOfTheHill: {
       name: 'KingOfTheHill',
