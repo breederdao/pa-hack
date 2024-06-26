7552a6344b5345d0270a772837eefd764be82e3d
johnpaulcas <casjp.app@gmail.com>
2024-06-27 07:42:30 +0800

    :recycle: Impl Base_Script and cleanup

--- packages/contracts/script/GetTimeControlled.s.sol
+++ packages/contracts/script/GetTimeControlled.s.sol
@@ -3,42 +3,22 @@ pragma solidity >=0.8.20;
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 
-import { IKingOfTheHill } from "../src/codegen/world/IKingOfTheHill.sol";
-import { KingOfTheHillConfig, KingOfTheHillStatus } from "../src/codegen/index.sol";
-
 import { IWorld } from "../src/codegen/world/IWorld.sol";
-import { IAreaControlLobby } from "../src/codegen/world/IAreaControlLobby.sol";
 
-import { KOTH_NAMESPACE } from "../src/systems/constants.sol";
+import { Base_Script } from "./Base.s.sol";
 import { AreaControlPoint } from "../src/systems/area_control/AreaControlPoint.sol";
 
-import { Utils as KothUtils } from "../src/systems/area_control/Utils.sol";
-
-contract GetTimeControlled is Script {
-    using KothUtils for bytes14;
-
-    function run(address worldAddress) external {
-        IWorld world = IWorld(worldAddress);
-
-        uint256 privatePk = vm.envUint("PRIVATE_KEY");
-        address owner = vm.addr(privatePk);
-        vm.startBroadcast(owner);
-
-        console.log("world address:", worldAddress);
-        console.log("Owner:", owner);
-
-        // get ssu_id on env file
-        uint256 _lobbySmartObjectId = vm.envUint("SSU_ID");
+contract GetTimeControlled is Base_Script {
+    function _run(IWorld world) internal override broadcastPlayer {
+        // Load Area Point SSU ID from env file
         uint256 _smartObjectId = vm.envUint("AP_SSU_ID");
-        console.log("smartStorageUnitId", _lobbySmartObjectId);
-        console.log("pointId", _smartObjectId);
 
         (uint256 teamATime, uint256 teamBTime) = abi.decode(
             world.call(
-                KOTH_NAMESPACE.pointSystemId(),
+                pointSystemId(),
                 abi.encodeCall(
                     AreaControlPoint.getTimeControlled,
-                    (_lobbySmartObjectId, _smartObjectId)
+                    (ssuIdLobby, _smartObjectId)
                 )
             ),
             (uint256, uint256)
@@ -46,7 +26,5 @@ contract GetTimeControlled is Script {
 
         console.log("teamATime", teamATime);
         console.log("teamBTime", teamBTime);
-
-        vm.stopBroadcast();
     }
 }
