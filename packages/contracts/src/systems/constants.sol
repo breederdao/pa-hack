dcc2c13255e03df8d7555d36d9165dde4b25a0c2
johnpaulcas <casjp.app@gmail.com>
2024-06-27 07:22:00 +0800

    :sparkles: Add KING_OF_THE_HILL constant

--- packages/contracts/src/systems/constants.sol
+++ packages/contracts/src/systems/constants.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.24;
 
 bytes16 constant AC_LOBBY_SYSTEM_NAME = "AreaControlLobby";
 bytes16 constant AC_POINT_SYSTEM_NAME = "AreaControlPoint";
+bytes16 constant KING_OF_THE_HILL = "KingOfTheHill";
 
 // ** System Name */
 
